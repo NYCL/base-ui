480e3c6e71af9cfd772eb85f2fb40606e6b6e0ea
NYCL <6975703+NYCL@users.noreply.github.com>
2026-04-14 17:25:55 -0500

    feat(vue): export checkbox, switch, and radio components

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,2 +1,7 @@
 export * from './field';
 export * from './input';
+export * from './checkbox';
+export * from './checkbox-group';
+export * from './switch';
+export * from './radio';
+export * from './radio-group';
